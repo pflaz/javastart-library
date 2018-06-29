a6a50efb0c31c1dbc45b24f5bc9e76bf5848b08c
pflaz <pf@szach.net>
2018-06-29 05:50:28 +0200

    Option as inner enum

--- library/src/main/java/app/LibraryControl.java
+++ library/src/main/java/app/LibraryControl.java
@@ -74,4 +74,34 @@ public class LibraryControl {
         LibraryUtils.printMagazines(library);
     }
 
+    private enum Option {
+        EXIT(0, "Wyjście z programu"),
+        ADD_BOOK(1, "Dodanie książki"),
+        ADD_MAGAZINE(2, "Dodanie magazynu"),
+        PRINT_BOOKS(3, "Wyświetlenie książek"),
+        PRINT_MAGAZINES(4, "Wyświetlenie magazynów");
+
+        private int value;
+        private String description;
+
+        Option(int value, String description) {
+            this.value = value;
+            this.description = description;
+        }
+
+        @Override
+        public String toString() {
+            return value + " - " + description;
+        }
+
+        public static Option createFromInt(int option) throws NoSuchElementException {
+            Option result = null;
+            try {
+                result = Option.values()[option];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new NoSuchElementException("Brak elementu o wskazanym ID");
+            }
+            return result;
+        }
+    }
 }
